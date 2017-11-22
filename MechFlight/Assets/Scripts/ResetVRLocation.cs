21103b7b019c41e99d1078561a4e98a8f582b46c
Herro-Sama <sandylily66@hotmail.com>
2017-11-22 16:38:27 +0000

    Updated game to include the prefabs for everything and to work.

---
+++ MechFlight/Assets/Scripts/ResetVRLocation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetVRLocation : MonoBehaviour {
+
+
+	public GameObject Spawn;
+
+	// Use this for initialization
+	void Start () 
+	{
+		gameObject.transform.SetPositionAndRotation (Spawn.transform.position, Spawn.transform.rotation);
+	}
+
+}
