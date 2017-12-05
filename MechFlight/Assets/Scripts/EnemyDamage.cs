2d898870e79a7becce8d3bf85b20f8410a92b9a8
Herro-Sama <sandylily66@hotmail.com>
2017-12-05 13:39:14 +0000

    Added a cockpit, from turbosquid. This is placeholder for now but may end up being the final version. Have the camera location locked currently to the cockpit. Should fix the VR movement issue.

---
+++ MechFlight/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+
+	private float Health = 50;
+
+
+
+	void OnTriggerEnter (Collider BulletCollider) 
+	{
+		if (BulletCollider.name == "Bullet(Clone)")
+		{
+			if (Health <= 0)
+			{
+				Destroy (gameObject);
+			}
+
+			else
+			{
+				Health--;
+				Destroy(BulletCollider);
+			}
+		}
+	}
+
+}
